f2d639e122c08f08bd0c717965d74ffdfed392f5
ahmednour <nourelnaby4@gmail.com>
2023-11-05 17:04:24 +0200

    Create Sidebar

---
+++ TeamTracker.SYS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TeamTracker.SYS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
